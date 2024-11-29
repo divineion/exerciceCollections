fe6abd1aa84a630d1cb25383dca48a64f8c7de27
divineion <najima.guermoudi@gmail.com>
2024-11-29 22:58:02 +0100

    src/fr.divineion.cartes/TestCartes.java :
    class used to :
     - test instantiations of Carte objects in HashSets and ArrayLists
    collections;
     - verify hashCode() and equals() methods in the class Carte

---
+++ src/fr/divineion/cartes/TestCartes.java
@@ -0,0 +1,56 @@
+package fr.divineion.cartes;
+
+public class TestCartes {
+
+	public static void main(String[] args) {
+
+		// tests with ArrayLists and HashSets containing cards
+		//
+//		
+//		cartes.add(new Carte(10, Couleur.CARREAU));
+//		
+//		if (cartes.contains(new Carte(10, Couleur.CARREAU))) {
+//		System.out.println("Le jeu contient le 10 de carreau");
+//		} else {
+//		System.out.println("Le jeu ne contient pas le 10 de carreau");
+//		}
+//		
+//		System.out.println(cartes);
+//
+//		Carte dixdecarreau = new Carte(10, Couleur.CARREAU);
+//		
+//		System.out.println("ajout de la carte deuxième dix de carreau : " + cartes.add(dixdecarreau));
+//		
+//		if (cartes.contains(dixdecarreau)) {
+//			System.out.println("Le jeu contient l'instance nommée dixdecarreau");
+//		} else {
+//			System.out.println("Le jeu ne contient pas le deuxième 10 de carreau");
+//		}
+//		
+//		if (dixdecarreau.equals(new Carte(10, Couleur.CARREAU))) {
+//			System.out.println("égalité");
+//		} else {
+//			System.out.println("equals ne renvoie pas l'égalité");
+//		}
+//		System.out.println(cartes);
+//
+//		System.out.println("ajout d'un nouveau 10 de carreau : " + cartes.add(new Carte(10, Couleur.CARREAU)));
+//		
+//		System.out.println(cartes);
+//
+//		
+//		System.out.println(Objects.hash(Couleur.CARREAU, 5));
+		
+//		MainJoueur1 main1 = new MainJoueur1();
+//		main1.add(new Carte(5, Couleur.CARREAU.toString()));
+//		main1.add(new Carte(5, Couleur.CARREAU.toString()));
+//		main1.add(new Carte(6, Couleur.PIQUE.toString()));
+//		main1.add(new Carte(10, Couleur.PIQUE.toString()));
+//		main1.add(new Carte(5, "CARREAU"));
+//		main1.add(new Carte(12, "carreau"));
+//		main1.add(new Carte(10, "CARREAU"));
+//		
+//		System.out.println("La main 1 contient le 8 de carreau : " + main1.contient(new Carte(8, "carreau")));
+//				
+	}
+}
