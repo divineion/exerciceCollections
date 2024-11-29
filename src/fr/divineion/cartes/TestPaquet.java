85f207c8dedcdea136be1ba806406ed142595f48
divineion <najima.guermoudi@gmail.com>
2024-11-29 22:47:17 +0100

    src/fr.divineion.cartes/TestPaquet.java :
    main() method to test the Paquet and MainJoueur1 classes :
     - instanciate a Paquet object (complete deck), shuffle the deck and
    create a player's hand using the creerMain1() method from the class
    Paquet; display the hand in the console;
     - add user interaction via Scanner to:
      - input an int card value;
      - input a string card suit;
     - validate user input :
      - handle invalid suits with IllegalArgumentException and throw a
    custom Exception IllegalCardValueException;
      - handle non-integer values with InputMismatchException and throw a
    custom Exception IllegalCardValueException;
     - check if the entered card exists in the player's hand and display the
    result.

---
+++ src/fr/divineion/cartes/TestPaquet.java
@@ -0,0 +1,38 @@
+package fr.divineion.cartes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import fr.divineion.cartes.Carte.Couleur;
+
+public class TestPaquet {
+	public static void main(String[] args) throws IllegalCardValueException {
+		Paquet paquet = new Paquet();
+		paquet.melanger();
+		MainJoueur1 maMain = paquet.creerMain1();
+		System.out.println("mon jeu " + maMain);
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Entrez une valeur de carte entre 1 (As) et 13 (Roi) :");
+			//TODO handle InputMismatchExc
+			try {
+				int valeur = scanner.nextInt();
+				System.out.print("Entrez une couleur de carte (\"pique\", \"coeur\", \"carreau\" ou \"trefle\") :");
+			
+				String couleurSaisie = scanner.next();
+			
+				Couleur couleur = Couleur.valueOf(couleurSaisie.toUpperCase());
+				Carte carte = new Carte(valeur, couleur);
+				if (maMain.contient(carte)) {
+					System.out.println("La main contient la carte");
+				} else
+					System.out.println("La main ne contient pas la carte");
+				} 
+			catch(IllegalArgumentException e) {
+				throw new IllegalCardValueException("Les couleurs admises sont \"pique\", \"coeur\", \"carreau\" et \"trefle\".");
+			}
+			catch(InputMismatchException e) {
+				throw new IllegalCardValueException("La valeur saisie doit être un nombre entre 1 (As) et 13 (Roi)");
+			}
+		}	
+	}
+}
